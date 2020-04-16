6ccdab9649ae75e460e6605728e5b1f4f90021f2
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-04-15 22:30:06 -0300

    Debugando NodeJS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,13 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.post('/users', (request, reponse) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+  return reponse.json(user);
+});
+
 export default routes;
