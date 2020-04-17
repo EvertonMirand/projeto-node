efe9db539541c3facf2768f49766e55e9c948ac3
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-04-16 21:23:17 -0300

    Trabalhando com dados

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentRepository {
   private appointments: Appointment[];
 
@@ -19,7 +24,7 @@ class AppointmentRepository {
     return findAppointment || null;
   }
 
-  public create(provider: string, date: Date): Appointment {
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
     const appointment = new Appointment(provider, date);
 
     this.appointments.push(appointment);
