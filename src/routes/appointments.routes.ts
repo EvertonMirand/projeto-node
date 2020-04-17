ec981e0a8e35782f4eb52118fc3079c4438e5518
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-04-16 21:12:47 -0300

    Listando Agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -8,6 +8,12 @@ const appointmentsRouter = Router();
 
 const appointmentRepository = new AppointmentRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
