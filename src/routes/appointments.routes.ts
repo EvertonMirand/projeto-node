2ea178dd5dc083015c98182c809f91a813274f9a
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-04-18 12:59:31 -0300

    Services & SOLID

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 
 import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../service/CreateAppointmentSerivice';
 
 const appointmentsRouter = Router();
 
@@ -15,23 +16,22 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointmentRepository.findByDate(
-    parsedDate,
-  );
-
-  if (findAppointmentInSameDate) {
-    return response.status(400).json({
-      message: 'This appointment is already booked',
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    );
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
     });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-  const appointment = appointmentRepository.create({
-    provider,
-    date: parsedDate,
-  });
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
