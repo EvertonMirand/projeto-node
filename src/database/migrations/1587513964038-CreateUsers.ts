1e0dca38c30d220bfe2daa525514e22adf4a1c4d
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-05-02 18:16:24 -0300

    Relacionamento nos models

--- src/database/migrations/1587513964038-CreateUsers.ts
+++ src/database/migrations/1587513964038-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587513964038 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
