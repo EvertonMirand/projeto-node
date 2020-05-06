a2fe3aabb55fb6fd287d17ef1a4a9b22ee56a95c
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-05-05 21:54:11 -0300

    Lidando com erros

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,4 @@
-import { Router, request } from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from '../config/upload';
@@ -10,23 +10,17 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUser = new CreateUserSevice();
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserSevice();
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.send(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({
-      error: err.message,
-    });
-  }
+  return response.send(user);
 });
 
 usersRouter.patch(
@@ -34,19 +28,13 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
-      delete user.password;
-      return response.json(user);
-    } catch (err) {
-      return response.status(err.statusCode).json({
-        error: err.message,
-      });
-    }
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
+    delete user.password;
+    return response.json(user);
   },
 );
 
